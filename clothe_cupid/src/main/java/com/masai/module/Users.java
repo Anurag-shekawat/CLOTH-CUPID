8906c7947753443cc109e7796cd7651af861d0a7
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2022-12-14 22:02:54 +0530

    login update

--- clothe_cupid/src/main/java/com/masai/module/Users.java
+++ clothe_cupid/src/main/java/com/masai/module/Users.java
@@ -2,9 +2,10 @@ package com.masai.module;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
