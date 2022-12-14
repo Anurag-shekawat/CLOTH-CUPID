2d7637d40194536e4103141f21492e6df80ff36b
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2022-12-14 23:41:26 +0530

    pending service added for the order section

--- clothe_cupid/src/main/java/com/masai/controller/OrderController.java
+++ clothe_cupid/src/main/java/com/masai/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.masai.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,55 +18,69 @@ import com.masai.exception.OrderException;
 import com.masai.module.OrderDetails;
 import com.masai.service.OrderService;
 
-
-
-@RestController	
+@RestController
 public class OrderController {
-    
-    @Autowired
-    private OrderService os;
 
-    @PostMapping("/AddOrder")
-    public ResponseEntity<OrderDetails> addOrder(@RequestBody OrderDetails order) throws OrderException{
+	@Autowired
+	private OrderService os;
 
-        OrderDetails AddOrder = os.addOrder(order);
-        
-        return new ResponseEntity<OrderDetails>(AddOrder,HttpStatus.CREATED);
-    }
+	@PostMapping("/AddOrder")
+	public ResponseEntity<OrderDetails> addOrder(@RequestBody OrderDetails order) throws OrderException {
 
+		OrderDetails AddOrder = os.addOrder(order);
 
-    @PutMapping("/updateOrder/{id}")
-	public ResponseEntity<OrderDetails> updateCustomer( @RequestBody OrderDetails order, @PathVariable("id") Integer id)throws OrderException{
-		
-		OrderDetails updatedOrder=os.updateOrder(order,id);
-		
-		return new ResponseEntity<OrderDetails>(updatedOrder,HttpStatus.OK);
+		return new ResponseEntity<OrderDetails>(AddOrder, HttpStatus.CREATED);
 	}
-	
+
+	@PutMapping("/updateOrder/{id}")
+	public ResponseEntity<OrderDetails> updateCustomer(@RequestBody OrderDetails order, @PathVariable("id") Integer id)
+			throws OrderException {
+
+		OrderDetails updatedOrder = os.updateOrder(order, id);
+
+		return new ResponseEntity<OrderDetails>(updatedOrder, HttpStatus.OK);
+	}
+
 	@DeleteMapping("/removeOrder/{id}")
-	public ResponseEntity<OrderDetails> removeCustomer(@PathVariable("id") Integer orderId)throws OrderException{
-		
-		OrderDetails removedOrder=os.removeOrder(orderId);
-		
-		return new ResponseEntity<OrderDetails>(removedOrder,HttpStatus.ACCEPTED);
+	public ResponseEntity<OrderDetails> removeCustomer(@PathVariable("id") Integer orderId) throws OrderException {
+
+		OrderDetails removedOrder = os.removeOrder(orderId);
+
+		return new ResponseEntity<OrderDetails>(removedOrder, HttpStatus.ACCEPTED);
 	}
-	
+
 	@GetMapping("/viewOrder/{id}")
-	public ResponseEntity<OrderDetails> viewOrder(@PathVariable("id") Integer orderId)throws OrderException{
-		
-		OrderDetails order=os.viewOrder(orderId);
-		
-		return new ResponseEntity<OrderDetails>(order,HttpStatus.ACCEPTED);
+	public ResponseEntity<OrderDetails> viewOrder(@PathVariable("id") Integer orderId) throws OrderException {
+
+		OrderDetails order = os.viewOrder(orderId);
+
+		return new ResponseEntity<OrderDetails>(order, HttpStatus.ACCEPTED);
 	}
 
 	@GetMapping("/viewAllOrder")
-	public ResponseEntity<List<OrderDetails>> viewAllOrder()throws OrderException{
-		
+	public ResponseEntity<List<OrderDetails>> viewAllOrder() throws OrderException {
+
 		List<OrderDetails> orders = os.getAllOrderDetails();
-		
-		return new ResponseEntity<List<OrderDetails>>(orders,HttpStatus.OK);
+
+		return new ResponseEntity<List<OrderDetails>>(orders, HttpStatus.OK);
 	}
 
-    
+	@GetMapping("/viewAllOrder/{date}")
+	public ResponseEntity<List<OrderDetails>> getOrderDetailsByDate(@PathVariable("date") LocalDate date)
+			throws OrderException {
+
+		List<OrderDetails> orders = os.getOrderDetailsByDate(date);
+
+		return new ResponseEntity<List<OrderDetails>>(orders, HttpStatus.OK);
+	}
+
+	@GetMapping("/viewOrders/{c_id}")
+	public ResponseEntity<List<OrderDetails>> getOrderDetailsByCustomerId(@PathVariable("c_id") Integer id)
+			throws OrderException {
+
+		List<OrderDetails> orders = os.getOrderDetailsByCustomerId(id);
+
+		return new ResponseEntity<List<OrderDetails>>(orders, HttpStatus.OK);
+	}
 
 }
