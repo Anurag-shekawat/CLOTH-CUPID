f1be5524abca8b3dff94b47d69cfe3a4d9db31ef
Akash07032001 <ak9821531375@gmail.com>
2022-12-15 11:30:45 +0530

    day2

---
+++ clothe_cupid/src/main/java/com/masai/exception/ProductException.java
@@ -0,0 +1,17 @@
+package com.masai.exception;
+
+public class ProductException extends Exception {
+
+	
+	public ProductException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ProductException(String msg){
+		super(msg);
+		
+	}
+	
+	
+
+}
