e4dab049f29c632df0464e9f0aef4b49a68ed869
Akash07032001 <ak9821531375@gmail.com>
2022-12-15 21:22:26 +0530

    day3
    sdfgsd

---
+++ clothe_cupid/src/main/java/com/masai/repository/ProductDao.java
@@ -0,0 +1,15 @@
+package com.masai.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.masai.module.Product;
+@Repository
+public interface ProductDao extends JpaRepository<Product, Integer> {
+	
+	public List<Product> findAllByCategory(String category);
+	
+
+}
---
+++ clothe_cupid/src/main/java/com/masai/service/ProductServiceImpl.java
@@ -0,0 +1,97 @@
+package com.masai.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.masai.exception.ProductException;
+import com.masai.module.Product;
+import com.masai.repository.ProductDao;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+
+	@Autowired
+	private ProductDao pDao;
+	
+	@Override
+	public List<Product> viewAllProducts() throws ProductException {
+		
+		    List<Product> list= pDao.findAll();
+		    
+		    if(list.size()==0) {
+		       throw new ProductException("Product DataBase is EMPTY!!!!");
+		    }
+		
+		return list;
+	}
+
+	@Override
+	public Product addProduct(Product product) throws ProductException {
+		
+		pDao.save(product);
+		
+		return product;
+	}
+
+	@Override
+	public Product updateProduct(Product product) throws ProductException {
+		
+		Optional<Product> opt=pDao.findById(product.getProductId());
+		
+		if(opt.isPresent()) {
+			
+			pDao.save(product);
+			
+			return product;
+			
+		}
+		
+		throw new ProductException("Product not found....!!!!!!!!");
+	}
+
+	@Override
+	public Product viewProduct(Integer pId) throws ProductException {
+		Optional<Product> opt=pDao.findById(pId);
+		
+		if(opt.isPresent()) {
+			Product product=opt.get();
+			
+			return product;
+		}
+		
+		throw new ProductException("Product not found....!!!!!!!!");
+	}
+
+	@Override
+	public List<Product> viewAllProductsByCategory(String cname) throws ProductException {
+		
+		List<Product> list=pDao.findAllByCategory(cname);
+		
+		if(list.size()==0) {
+			throw new ProductException("Product not found....!!!!!!!!");
+		}
+		
+		
+		
+		return list;
+	}
+
+	@Override
+	public Product removeProduct(Integer pId) throws ProductException {
+		
+         Optional<Product> opt=pDao.findById(pId);
+		
+		if(opt.isPresent()) {
+			Product pro=opt.get();
+			pDao.delete(pro);
+			
+			return pro;
+		}
+		
+		throw new ProductException("Product not found....!!!!!!!!");
+	}
+
+}
