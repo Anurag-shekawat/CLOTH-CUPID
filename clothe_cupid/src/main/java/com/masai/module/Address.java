8b3371a52833b60b1ea6ba05986e2370cfe98a7f
Anurag-shekawat <anuragshekhawat806@gmail.com>
2022-12-18 13:36:40 +0530

    validation done

--- clothe_cupid/src/main/java/com/masai/module/Address.java
+++ clothe_cupid/src/main/java/com/masai/module/Address.java
@@ -6,8 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,16 +23,23 @@ public class Address {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer addressId;
 
+	@NotNull(message = "streetNo can not be null")
 	private String streetNo;
 
+	@NotNull(message = "building name can not be null")
 	private String buildingName;
 
+	@NotNull(message = "city cannot can not be null")
 	private String city;
 
+	@NotNull(message = "state can not be null")
 	private String state;
 
+	@NotNull(message = "country can not be null")
 	private String country;
 
+	@NotNull(message = "pincode can not be null")
+	@Max(value = 6,message = "pincode size cannot be greater then 6")
 	private String pincode;
 
 	@OneToOne(cascade = CascadeType.ALL)
--- clothe_cupid/src/main/java/com/masai/service/CustomerService.java
+++ clothe_cupid/src/main/java/com/masai/service/CustomerService.java
@@ -1,5 +1,6 @@
 package com.masai.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
