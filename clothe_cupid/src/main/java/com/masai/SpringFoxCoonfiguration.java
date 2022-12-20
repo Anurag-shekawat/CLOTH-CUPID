24ca121cbc1d665c5283bf297b64fb3f61370cab
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2022-12-20 11:48:35 +0530

    spring configuration added

--- clothe_cupid/src/main/java/com/masai/SpringFoxCoonfiguration.java
+++ clothe_cupid/src/main/java/com/masai/SpringFoxCoonfiguration.java
@@ -16,14 +16,31 @@ public class SpringFoxCoonfiguration {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().paths(PathSelectors.any())
-				.apis(RequestHandlerSelectors.basePackage("com.masai")).build().apiInfo(apiDetails());
+		return new Docket(DocumentationType.SWAGGER_2)
+        .apiInfo(getInfo()).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
 	}
 
-	private ApiInfo apiDetails() {
-		return new ApiInfo("Online Shoping application", "Api for shoping application", "1.0", "Free to use",
-				new springfox.documentation.service.Contact(null, null, null), "Github",
-				"https://github.com/Anurag-shekawat/main-transport-9463", Collections.emptyList());
-	}
+	// private ApiInfo apiDetails() {
+	// 	return new ApiInfo("Online Shoping application", "Api for shoping application", "1.0", "Free to use",
+	// 			new springfox.documentation.service.Contact(null, null, null), "Github",
+	// 			"https://github.com/Anurag-shekawat/main-transport-9463", Collections.emptyList());
+	// }
+	private ApiInfo getInfo() {
+        return new ApiInfo("Cloth Cupid : Online Shopping Application", 
+                            "This project is developed by Masai School Students \n"+
+                             "Team Name - main-transport-9463 \n"+
+                             " Team Members : \n"+
+                             " ------------------- \n"+
+                             "Anurag Shekhawat \n"+
+                             "Vinay Pathania \n"+
+                             "Suhaib Malik \n"+
+                             "Shubham Singh \n"+
+                             "Akash Kumar", 
+                            "1.0", "Term of Services", 
+                            new springfox.documentation.service.Contact("Team main-transport-9463", "https://github.com/Anurag-shekawat/main-transport-9463", "abc@gmail.com"), 
+                            "Lisence of API", 
+                            "https://github.com/Anurag-shekawat/main-transport-9463", 
+                            Collections.emptyList());
+    }
 
 }
